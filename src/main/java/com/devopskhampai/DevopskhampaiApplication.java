c85ceadb8f32d57f3912afa984b27734b7f94633
khampaii <mail2horen@gmail.com>
2019-04-22 18:44:30 +0530

    Initial commit for spring project

---
+++ src/main/java/com/devopskhampai/DevopskhampaiApplication.java
@@ -0,0 +1,13 @@
+package com.devopskhampai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopskhampaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopskhampaiApplication.class, args);
+	}
+
+}
